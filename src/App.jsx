96a3503205eb2290ad8a1e7ddb11bff40a8d6209
Mathias42lm <mathias.mellier@gmail.com>
2026-04-30 11:11:26 +0200

    Modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem', textAlign: 'center' }}>
-      <h1>🚀 Déploiement automatique fontionnelle</h1>
+      <h1>🚀 Déploiement automatique fontionnelle 🚀</h1>
       <p>Projet réalisé par <strong>Mathias Mellier</strong></p>
       <p>Future application : gestion de tâches Kanban</p>
       <p>Github : https://github.com/Mathias42lm/R2.09</p>
