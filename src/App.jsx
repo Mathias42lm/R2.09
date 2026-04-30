43198b50f8b4e3da7316ff68fa477093a4598a24
Mathias42lm <mathias.mellier@gmail.com>
2026-04-30 11:05:33 +0200

    Modify validation and app.lsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1>🚀 Déploiement automatique fontionnelle</h1>
       <p>Projet réalisé par <strong>Mathias Mellier</strong></p>
       <p>Future application : gestion de tâches Kanban</p>
+      <p>Github : https://github.com/Mathias42lm/R2.09</p>
     </div>
   );
 }
