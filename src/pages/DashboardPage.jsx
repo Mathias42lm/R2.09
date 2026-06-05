64f972e0b1ece3e9de16670f83f44531b06820b3
Mathias42lm <mathias.mellier@gmail.com>
2026-06-05 11:51:15 +0200

    modif things

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import UserTable from '../components/UserTable';
 import TaskList from '../components/TaskList';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 
 export default function DashboardPage({ session }) {
   const [users, setUsers] = useState([]);
